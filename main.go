259c17eda1afa43865239902606b9c66b16876d7
Warren Wang <warren.wang2020@gmail.com>
2023-04-21 14:03:17 -0400

    maps work, now WIP on caching the results

--- main.go
+++ main.go
@@ -6,18 +6,47 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+// NOTE: maps are NOT thread safe
+func (cache) add(key string, val []byte) {
+	// TODO:
+}
+
+func (cache) get(key string) ([]byte, bool) {
+	// TODO:
+	return nil, false
+}
+
+func (cache) reapleap() {
+	// TODO:
+}
 
 type Command struct {
 	name        string
 	description string
-	callback    func() error
+	callback    Callback
 }
 
-type CommandWithParam struct {
-	name        string
-	description string
-	callback    func(config *mapConfig) error
+type Callback interface {
+	Execute(args ...interface{}) error
+}
+
+type NoParamFunc func() error
+type ParamFunc func(args ...interface{}) error
+
+func (f NoParamFunc) Execute(args ...interface{}) error {
+	return f()
+}
+
+func (f ParamFunc) Execute(args ...interface{}) error {
+	return f(args...)
 }
 
 type LocationAreas struct {
@@ -30,7 +59,7 @@ type LocationAreas struct {
 	} `json:"results"`
 }
 
-type mapConfig struct {
+type MapConfig struct {
 	Next     *string `json:"next"`
 	Previous *string `json:"previous"`
 }
@@ -48,8 +77,9 @@ func helpCommand() error {
 // use pokedex API to get the names of 20 location areas
 // make a GET request to the API here: https://pokeapi.co/api/v2/location-area/
 // and print the names of the 20 location areas
-func mapCommand(config mapConfig) error {
-	url := "https://pokeapi.co/api/v2/location-area/"
+func mapCommand(args ...interface{}) error {
+	mapConfig := args[0].(*MapConfig)
+	url := *mapConfig.Next
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
@@ -68,11 +98,44 @@ func mapCommand(config mapConfig) error {
 		fmt.Println(locationArea.Name)
 	}
 
+	// update the mapConfig next and previous fields
+	mapConfig.Next = &locationAreas.Next
+	mapConfig.Previous = &locationAreas.Previous
+
 	return nil
 }
 
-func mapbCommand() error {
-	// TODO: use pokedex API to get the names of 20 location areas
+// get the names of the previous 20 location areas
+func mapbCommand(args ...interface{}) error {
+	mapConfig := args[0].(*MapConfig)
+	// if no previous page, return an error
+	if mapConfig.Previous == nil || *mapConfig.Previous == "" {
+		return fmt.Errorf("no previous page")
+	}
+	url := *mapConfig.Previous
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// decode the response body into a struct
+	var locationAreas LocationAreas
+	err = json.NewDecoder(resp.Body).Decode(&locationAreas)
+	if err != nil {
+		return err
+	}
+
+	// print the names of the 20 location areas
+	for _, locationArea := range locationAreas.Results {
+		fmt.Println(locationArea.Name)
+	}
+
+	// update the mapConfig next and previous fields
+	mapConfig.Next = &locationAreas.Next
+	mapConfig.Previous = &locationAreas.Previous
+
 	return nil
 }
 
@@ -82,31 +145,35 @@ func main() {
 	cmdHandler["help"] = Command{
 		name:        "help",
 		description: "Show help",
-		callback:    helpCommand,
+		callback:    NoParamFunc(helpCommand),
 	}
 
 	cmdHandler["exit"] = Command{
 		name:        "exit",
 		description: "Exit the CLI",
-		callback:    func() error { os.Exit(0); return nil },
+		callback:    NoParamFunc(func() error { os.Exit(0); return nil }),
 	}
 
-	mapConfig := mapConfig{
-		Next:     nil,
+	initMapURL := "https://pokeapi.co/api/v2/location-area/"
+	mapConfig := MapConfig{
+		Next:     &initMapURL,
 		Previous: nil,
 	}
 
 	cmdHandler["map"] = Command{
 		name:        "map",
 		description: "Displays the names of the next 20 location areas",
-		callback:    mapCommand,
+		callback:    ParamFunc(mapCommand),
 	}
 
 	cmdHandler["mapb"] = Command{
 		name:        "map",
 		description: "Displays the names of the previous 20 location areas",
-		callback:    mapbCommand,
+		callback:    ParamFunc(mapbCommand),
 	}
+
+	// cache for maps
+	cache := make(map[string]cacheEntry)
 
 	// REPL loop
 	for {
@@ -116,19 +183,16 @@ func main() {
 		input.Scan()
 		cmd := input.Text()
 
-		switch cmd {
-		case "map":
-			mapCommand(&mapConfig)
-		case "mapb":
-			mapbCommand(&mapConfig)
-		default:
-			// try except
-			if cmdHandler[cmd].callback != nil {
-				cmdHandler[cmd].callback()
-			} else {
-				fmt.Println("Command not found")
+		// try except
+		if cmd == "map" || cmd == "mapb" {
+			err := cmdHandler[cmd].callback.Execute(&mapConfig)
+			if err != nil {
+				fmt.Println(err)
 			}
+		} else if cmdHandler[cmd].callback != nil {
+			cmdHandler[cmd].callback.Execute()
+		} else {
+			fmt.Println("Command not found")
 		}
-
 	}
 }
